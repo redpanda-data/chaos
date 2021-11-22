91dbc14c7daa485958884465bf0dd40514593c3c
Denis Rystsov <denis@vectorized.io>
2021-11-21 16:49:32 -0800

    rework workload dir structure

--- control/list-offsets.java.start.sh
+++ control/list-offsets.java.start.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd /mnt/vectorized/workloads/logs
-nohup java -cp /mnt/vectorized/workloads/writing/list-offsets/target/list-offsets-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/writing/list-offsets/target/dependency/* io.vectorized.App > /mnt/vectorized/workloads/logs/list-offsets.log 2>&1 & echo $! > /mnt/vectorized/workloads/logs/list-offsets.pid
+nohup java -cp /mnt/vectorized/workloads/list-offsets/target/list-offsets-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/list-offsets/target/dependency/* io.vectorized.App > /mnt/vectorized/workloads/logs/list-offsets.log 2>&1 & echo $! > /mnt/vectorized/workloads/logs/list-offsets.pid
