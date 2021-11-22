f98a77304d5d3a4c7cd51a099c1a5a205d0e35cd
Denis Rystsov <denis@vectorized.io>
2021-11-21 16:27:33 -0800

    rename chaos.workload.(writing to writes)

--- harness/chaos/workloads/all.py
+++ harness/chaos/workloads/all.py
@@ -1,40 +1,40 @@
-from chaos.workloads.writing import writing
+from chaos.workloads.writes import writes
 
 import logging
 
 logger = logging.getLogger("chaos")
 
 def kafka_clients_workload(nodes_path):
-    writing_java = writing.Control()
+    writing_java = writes.Control()
     writing_java.launch = "/mnt/vectorized/control/writing.kafka-clients.start.sh"
     writing_java.alive = "/mnt/vectorized/control/writing.kafka-clients.alive.sh"
     writing_java.kill = "/mnt/vectorized/control/writing.kafka-clients.stop.sh"
     writing_java.name = "writes / java"
-    return writing.Workload(writing_java, nodes_path)
+    return writes.Workload(writing_java, nodes_path)
 
 def confluent_kafka_workload(nodes_path):
-    writing_python = writing.Control()
+    writing_python = writes.Control()
     writing_python.launch = "/mnt/vectorized/control/writing.confluent-kafka.start.sh"
     writing_python.alive = "/mnt/vectorized/control/writing.confluent-kafka.alive.sh"
     writing_python.kill = "/mnt/vectorized/control/writing.confluent-kafka.stop.sh"
     writing_python.name = "writes / python"
-    return writing.Workload(writing_python, nodes_path)
+    return writes.Workload(writing_python, nodes_path)
 
 def list_offsets_workload(nodes_path):
-    writing_java = writing.Control()
+    writing_java = writes.Control()
     writing_java.launch = "/mnt/vectorized/control/writing.list-offsets.start.sh"
     writing_java.alive = "/mnt/vectorized/control/writing.list-offsets.alive.sh"
     writing_java.kill = "/mnt/vectorized/control/writing.list-offsets.stop.sh"
     writing_java.name = "list-offsets / java"
-    return writing.Workload(writing_java, nodes_path)
+    return writes.Workload(writing_java, nodes_path)
 
 def reads_writes_workload(nodes_path):
-    reads_writes = writing.Control()
+    reads_writes = writes.Control()
     reads_writes.launch = "/mnt/vectorized/control/writing.reads-writes.start.sh"
     reads_writes.alive = "/mnt/vectorized/control/writing.reads-writes.alive.sh"
     reads_writes.kill = "/mnt/vectorized/control/writing.reads-writes.stop.sh"
     reads_writes.name = "reads-writes / java"
-    return writing.Workload(reads_writes, nodes_path)
+    return writes.Workload(reads_writes, nodes_path)
 
 WORKLOADS = {
     "reads-writes / java": reads_writes_workload,
