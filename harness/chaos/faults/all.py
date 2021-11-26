c51a25a82e369b53dfe1307eca4b126de1372f06
Denis Rystsov <denis@vectorized.io>
2021-11-26 09:32:03 -0800

    add isolate_clients_kill_leader fault

--- harness/chaos/faults/all.py
+++ harness/chaos/faults/all.py
@@ -10,11 +10,13 @@ from chaos.faults.leadership_transfer import LeadershipTransferFault
 from chaos.faults.reconfigure_11_kill import Reconfigure11KillFault
 from chaos.faults.reconfigure_313 import Reconfigure313Fault
 from chaos.faults.reconfigure_kill_11 import ReconfigureKill11Fault
+from chaos.faults.isolate_clients_kill_leader import IsolateClientsKillLeader
 
 FAULTS = {
     "isolate_controller": IsolateControllerFault,
     "isolate_follower": IsolateFollowerFault,
     "isolate_leader": IsolateLeaderFault,
+    "isolate_clients_kill_leader": IsolateClientsKillLeader,
     "kill_follower": KillFollowerFault,
     "kill_leader": KillLeaderFault,
     "kill_all": KillAllFault,
---
+++ harness/chaos/faults/isolate_clients_kill_leader.py
@@ -0,0 +1,39 @@
+from time import sleep
+from sh import ssh
+import logging
+from chaos.faults.types import FaultType
+
+logger = logging.getLogger("chaos")
+
+class IsolateClientsKillLeader:
+    def __init__(self, fault_config):
+        self.fault_type = FaultType.RECOVERABLE
+        self.name = "isolate clients / kill leader"
+        self.leader = None
+        self.fault_config = fault_config
+    
+    def inject(self, scenario):
+        self.leader = scenario.redpanda_cluster.wait_leader(scenario.topic, partition=scenario.partition, timeout_s=10)
+        logger.debug(f"isolating clients from redpanda cluster")
+
+        redpanda_nodes = []
+        for node in scenario.redpanda_cluster.nodes:
+            redpanda_nodes.append(node.ip)
+        
+        for node in scenario.workload_cluster.nodes:
+            ssh("ubuntu@"+node.ip, "/mnt/vectorized/control/network.isolate.sh", *redpanda_nodes)
+        
+        sleep(self.fault_config["kill_delay_s"])
+
+        self.leader = scenario.redpanda_cluster.wait_leader(scenario.topic, partition=scenario.partition, timeout_s=10)
+        logger.debug(f"killing {scenario.topic}'s leader: {self.leader.ip}")
+        ssh("ubuntu@"+self.leader.ip, "/mnt/vectorized/control/redpanda.stop.sh")
+
+        sleep(self.fault_config["reconnect_delay_s"])
+
+        logger.debug(f"reconnecting clients to redpanda cluster")
+        for node in scenario.workload_cluster.nodes:
+            ssh("ubuntu@"+node.ip, "/mnt/vectorized/control/network.heal.sh", *redpanda_nodes)
+    
+    def heal(self, scenario):
+        ssh("ubuntu@"+self.leader.ip, "/mnt/vectorized/control/redpanda.start.sh")
