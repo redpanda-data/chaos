881c8c9af2c83802b53317aea202fdc303678166
Denis Rystsov <denis@vectorized.io>
2021-12-16 12:36:56 -0800

    update test suites use common faults

--- harness/chaos/faults/kill_follower.py
+++ harness/chaos/faults/kill_follower.py
@@ -9,13 +9,28 @@ class KillFollowerFault:
     def __init__(self, fault_config):
         self.fault_type = FaultType.RECOVERABLE
         self.follower = None
+        self.fault_config = fault_config
         self.name = "kill a topic's follower"
 
     def inject(self, scenario):
         controller = scenario.redpanda_cluster.wait_leader("controller", namespace="redpanda", timeout_s=10)
         logger.debug(f"controller's leader: {controller.ip}")
         
-        replicas_info = scenario.redpanda_cluster.wait_details(scenario.topic, partition=scenario.partition, timeout_s=10)
+        topic = None
+        if "topic" in self.fault_config:
+            topic = self.fault_config["topic"]
+        else:
+            topic = scenario.topic
+        partition = 0
+        if "partition" in self.fault_config:
+            partition = self.fault_config["partition"]
+        else:
+            partition = scenario.partition
+        namespace="kafka"
+        if "namespace" in self.fault_config:
+            namespace = self.fault_config["namespace"]
+
+        replicas_info = scenario.redpanda_cluster.wait_details(topic, partition=partition, namespace=namespace, timeout_s=10)
         if len(replicas_info.replicas)==1:
             raise Exception(f"topic {scenario.topic} has replication factor of 1: can't find a follower")
 
@@ -28,7 +43,7 @@ class KillFollowerFault:
             if replica != controller:
                 self.follower = replica
         
-        logger.debug(f"killing {scenario.topic}'s follower: {self.follower.ip}")
+        logger.debug(f"killing {namespace}/{topic}/{partition}'s follower: {self.follower.ip}")
         ssh("ubuntu@"+self.follower.ip, "/mnt/vectorized/control/redpanda.stop.sh")
     
     def heal(self, scenario):
