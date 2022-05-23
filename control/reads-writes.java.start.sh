4953b425fc4e4807d82b0745f1c3bd8394b97faa
Denis Rystsov <denis@vectorized.io>
2022-05-23 13:45:00 -0700

    add reads-writes to the single java tests project

--- control/reads-writes.java.start.sh
+++ control/reads-writes.java.start.sh
@@ -3,5 +3,5 @@
 set -e
 
 cd /mnt/vectorized/workloads/logs
-nohup java -cp /mnt/vectorized/workloads/reads-writes/target/reads-writes-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/reads-writes/target/dependency/* io.vectorized.App >/mnt/vectorized/workloads/logs/system.log 2>&1 &
+nohup java -cp /mnt/vectorized/workloads/uber/target/uber-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/uber/target/dependency/* io.vectorized.reads_writes.App >/mnt/vectorized/workloads/logs/system.log 2>&1 &
 echo $! >/mnt/vectorized/workloads/logs/reads-writes.pid
--- workloads/reads-writes/README.md
+++
@@ -1,9 +0,0 @@
-mvn clean dependency:copy-dependencies package
-
-java -cp target/reads-writes-1.0-SNAPSHOT.jar:target/dependency/* io.vectorized.App
-
-curl -X POST http://127.0.0.1:8080/init -H 'Content-Type: application/json' -d '{"topic":"topic1","brokers":"127.0.0.1:9092","experiment":"experiment1", "server":"server1", "settings": { "writes": 1, "reads": 1 }}'
-
-curl -X POST http://127.0.0.1:8080/start
-
-curl -X GET http://127.0.0.1:8080/info
