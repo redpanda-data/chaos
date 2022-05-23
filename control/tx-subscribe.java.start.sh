ed0d3220f4e2a18c93603212221205602f5eb095
Denis Rystsov <denis@vectorized.io>
2022-05-23 15:50:21 -0700

    add tx-subscribe to the single java tests project

--- control/tx-subscribe.java.start.sh
+++ control/tx-subscribe.java.start.sh
@@ -3,5 +3,5 @@
 set -e
 
 cd /mnt/vectorized/workloads/logs
-nohup java -cp /mnt/vectorized/workloads/tx-subscribe/target/tx-subscribe-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/tx-subscribe/target/dependency/* io.vectorized.App >/mnt/vectorized/workloads/logs/system.log 2>&1 &
+nohup java -cp /mnt/vectorized/workloads/uber/target/uber-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/uber/target/dependency/* io.vectorized.tx_subscribe.App >/mnt/vectorized/workloads/logs/system.log 2>&1 &
 echo $! >/mnt/vectorized/workloads/logs/tx-subscribe.pid
