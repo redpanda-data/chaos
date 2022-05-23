0a5b18c0b6af31069c2a883c0b8e8a2b802d5718
Denis Rystsov <denis@vectorized.io>
2022-05-23 14:04:29 -0700

    add tx-money to the single java tests project

--- control/tx-money.java.start.sh
+++ control/tx-money.java.start.sh
@@ -3,5 +3,5 @@
 set -e
 
 cd /mnt/vectorized/workloads/logs
-nohup java -cp /mnt/vectorized/workloads/tx-money/target/tx-money-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/tx-money/target/dependency/* io.vectorized.App >/mnt/vectorized/workloads/logs/system.log 2>&1 &
+nohup java -cp /mnt/vectorized/workloads/uber/target/uber-1.0-SNAPSHOT.jar:/mnt/vectorized/workloads/uber/target/dependency/* io.vectorized.tx_money.App >/mnt/vectorized/workloads/logs/system.log 2>&1 &
 echo $! >/mnt/vectorized/workloads/logs/tx-money.pid
--- workloads/uber/src/main/java/io/vectorized/tx_money/Workload.java
+++ workloads/uber/src/main/java/io/vectorized/tx_money/Workload.java
@@ -1,4 +1,4 @@
-package io.vectorized;
+package io.vectorized.tx_money;
 import java.io.*;
 import java.util.Properties;
 import org.apache.kafka.clients.producer.Producer;
