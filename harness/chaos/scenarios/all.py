65bfdefbb2076135f35545d26209ea4f183702b2
Denis Rystsov <denis@vectorized.io>
2022-05-23 13:07:07 -0700

    remove obsolete test

--- harness/chaos/scenarios/all.py
+++ harness/chaos/scenarios/all.py
@@ -1,7 +1,6 @@
 from chaos.scenarios.single_topic_single_fault import SingleTopicSingleFault
 from chaos.scenarios.tx_single_topic_single_fault import TxSingleTopicSingleFault
 from chaos.scenarios.tx_money_single_fault import TxMoneySingleFault
-from chaos.scenarios.tx_streaming_single_fault import TxStreamingSingleFault
 from chaos.scenarios.tx_subscribe_single_fault import TxSubscribeSingleFault
 from chaos.scenarios.tx_poll_single_fault import TxPollSingleFault
 
@@ -9,7 +8,6 @@ SCENARIOS = {
     "single_table_single_fault": SingleTopicSingleFault,
     "tx_single_table_single_fault": TxSingleTopicSingleFault,
     "tx_money_single_fault": TxMoneySingleFault,
-    "tx_streaming_single_fault": TxStreamingSingleFault,
     "tx_subscribe_single_fault": TxSubscribeSingleFault,
     "tx_poll_single_fault": TxPollSingleFault
 }
