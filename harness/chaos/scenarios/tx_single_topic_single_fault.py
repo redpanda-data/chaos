a0b37593425f96f4c42153e429dd053c745d78c5
Denis Rystsov <denis@vectorized.io>
2022-06-03 00:01:54 -0700

    improve tx_single_reads_writes error recognition

--- harness/chaos/scenarios/tx_single_topic_single_fault.py
+++ harness/chaos/scenarios/tx_single_topic_single_fault.py
@@ -1,4 +1,5 @@
-from chaos.scenarios.abstract_single_fault import AbstractSingleFault
+from chaos.scenarios.abstract_single_fault import AbstractSingleFault, ProgressException
+from chaos.types import TimeoutException
 from sh import mkdir
 from chaos.faults.all import FAULTS
 from chaos.workloads.all import WORKLOADS, wait_all_workloads_killed
@@ -106,7 +107,10 @@ class TxSingleTopicSingleFault(AbstractSingleFault):
         wait_progress_timeout_s = self.read_config(["settings", "setup", "wait_progress_timeout_s"], 20)
         
         logger.info(f"waiting for progress")
-        self.workload_cluster.wait_progress(timeout_s=wait_progress_timeout_s)
+        try:
+            self.workload_cluster.wait_progress(timeout_s=wait_progress_timeout_s)
+        except TimeoutException:
+            raise ProgressException()
         logger.info(f"waiting for id_allocator")
         self.redpanda_cluster.wait_leader("id_allocator", namespace="kafka_internal", replication=3, timeout_s=10)
         logger.info(f"waiting for tx coordinator")
@@ -127,7 +131,10 @@ class TxSingleTopicSingleFault(AbstractSingleFault):
         # reconfigure tx to use other nodes
         self._reconfigure(others, "tx", partition=0, namespace="kafka_internal", timeout_s=20)
         
-        self.workload_cluster.wait_progress(timeout_s=wait_progress_timeout_s)
+        try:
+            self.workload_cluster.wait_progress(timeout_s=wait_progress_timeout_s)
+        except TimeoutException:
+            raise ProgressException()
 
         # transfer controller to other[0]
         self._transfer(others[0], "controller", partition=0, namespace="redpanda", timeout_s=10)
@@ -138,7 +145,10 @@ class TxSingleTopicSingleFault(AbstractSingleFault):
         # transfer tx to other[2]
         self._transfer(others[2], "tx", partition=0, namespace="kafka_internal", timeout_s=10)
 
-        self.workload_cluster.wait_progress(timeout_s=wait_progress_timeout_s)
+        try:
+            self.workload_cluster.wait_progress(timeout_s=wait_progress_timeout_s)
+        except TimeoutException:
+            raise ProgressException()
         warmup_s = self.read_config(["settings", "setup", "warmup_s"], 20)
         if warmup_s > 0:
             logger.info(f"warming up for {warmup_s}s")
