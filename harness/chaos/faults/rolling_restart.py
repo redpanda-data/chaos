a73dfc9dd21a38a790cccc294b6a2d995261a74e
Denis Rystsov <denis@vectorized.io>
2022-06-12 13:14:10 -0700

    improve rolling_restart fault

--- harness/chaos/faults/rolling_restart.py
+++ harness/chaos/faults/rolling_restart.py
@@ -12,6 +12,21 @@ class RollingRestartFault:
         self.fault_config = fault_config
 
     def execute(self, scenario):
+        full = False
+        if "full" in self.fault_config:
+            full = self.fault_config["full"]
+        
+        if full:
+            for replica in scenario.redpanda_cluster.nodes:
+                logger.debug(f"killing {replica.ip}")
+                ssh("ubuntu@" + replica.ip, "/mnt/vectorized/control/redpanda.stop.sh")
+                logger.debug(f"starting {replica.ip}")
+                ssh("ubuntu@" + replica.ip, "/mnt/vectorized/control/redpanda.start.sh")
+                if replica != scenario.redpanda_cluster.nodes[-1]:
+                    sleep(self.fault_config["period_s"])
+            return
+        
+        
         replicas_info = scenario.redpanda_cluster.wait_details(scenario.topic, partition=scenario.partition, timeout_s=10)
         
         sequence = [ replicas_info.leader.ip ]
