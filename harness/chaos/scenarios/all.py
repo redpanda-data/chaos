1d82c310b773511058d9c033059884bb620811e1
Denis Rystsov <denis@vectorized.io>
2022-06-11 18:29:15 -0700

    removes tx_poll

--- harness/chaos/scenarios/all.py
+++ harness/chaos/scenarios/all.py
@@ -3,13 +3,11 @@ from chaos.scenarios.tx_single_topic_single_fault import TxSingleTopicSingleFaul
 from chaos.scenarios.tx_money_single_fault import TxMoneySingleFault
 from chaos.scenarios.tx_subscribe_single_fault import TxSubscribeSingleFault
 from chaos.scenarios.rw_subscribe_single_fault import RWSubscribeSingleFault
-from chaos.scenarios.tx_poll_single_fault import TxPollSingleFault
 
 SCENARIOS = {
     "single_table_single_fault": SingleTopicSingleFault,
     "tx_single_table_single_fault": TxSingleTopicSingleFault,
     "tx_money_single_fault": TxMoneySingleFault,
     "tx_subscribe_single_fault": TxSubscribeSingleFault,
-    "rw_subscribe_single_fault": RWSubscribeSingleFault,
-    "tx_poll_single_fault": TxPollSingleFault
+    "rw_subscribe_single_fault": RWSubscribeSingleFault
 }
