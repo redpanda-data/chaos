2cb8c2d895d3589fa903c5e84278b80b4a51a1bc
Denis Rystsov <denis@vectorized.io>
2022-06-12 20:12:28 -0700

    add retry setting

--- harness/test.suite.py
+++ harness/test.suite.py
@@ -43,12 +43,26 @@ if "settings" not in suite:
 tests = {}
 ignore_transient_errors = False
 at_least_one_passes = False
+should_retry_on_transient_errors = False
 
 if "ignore_transient_errors" in suite["settings"]["suite"]:
     ignore_transient_errors = suite["settings"]["suite"]["ignore_transient_errors"]
 
 if "at_least_one_passes" in suite["settings"]["suite"]:
     at_least_one_passes = suite["settings"]["suite"]["at_least_one_passes"]
+
+retries = 0
+if "should_retry_on_transient_errors" in suite["settings"]["suite"]:
+    should_retry_on_transient_errors = suite["settings"]["suite"]["should_retry_on_transient_errors"]
+    if should_retry_on_transient_errors:
+        if ignore_transient_errors:
+            raise Exception(f"ignore_transient_errors isn't compatible with should_retry_on_transient_errors")
+        if at_least_one_passes:
+            raise Exception(f"at_least_one_passes isn't compatible with should_retry_on_transient_errors")
+        if "retries" in suite["settings"]["suite"]:
+            retries = suite["settings"]["suite"]["retries"]
+        else:
+            raise Exception(f"retry should be provided when should_retry_on_transient_errors")
 
 for test_path in suite["tests"]:
     test_path = os.path.join(os.path.dirname(args.suite), "tests", test_path)
@@ -82,49 +96,68 @@ for i in range(0, args.repeat):
         if name not in results["test_runs"]:
             results["test_runs"][name] = {}
         
-        experiment_id = str(int(time.time()))
+        attempt = 0
+        passed = True
+        while attempt <= retries:
+            attempt += 1
+            experiment_id = str(int(time.time()))
 
-        mkdir("-p", f"/mnt/vectorized/experiments/{experiment_id}")
-        handler = FileHandler(f"/mnt/vectorized/experiments/{experiment_id}/log")
-        handler.setFormatter(formatter)
-        chaos_logger.addHandler(handler)
-        tasks_logger.addHandler(handler)
-        
-        test = tests[name]
-        try:
-            scenario = SCENARIOS[test["scenario"]]()
-            description = scenario.execute(test, experiment_id)
-            results["test_runs"][test["name"]][experiment_id] = description["result"]
-        except:
-            e, v = sys.exc_info()[:2]
-            trace = traceback.format_exc()
-            chaos_logger.error(v)
-            chaos_logger.error(trace)
-            results["test_runs"][test["name"]][experiment_id] = Result.UNKNOWN
-        finally:
-            handler.flush()
-            handler.close()
-            chaos_logger.removeHandler(handler)
-            tasks_logger.removeHandler(handler)
-        
-        most_severe_result = Result.more_severe(most_severe_result, results["test_runs"][test["name"]][experiment_id])
-        least_severe_result = Result.least_severe(least_severe_result, results["test_runs"][test["name"]][experiment_id])
+            mkdir("-p", f"/mnt/vectorized/experiments/{experiment_id}")
+            handler = FileHandler(f"/mnt/vectorized/experiments/{experiment_id}/log")
+            handler.setFormatter(formatter)
+            chaos_logger.addHandler(handler)
+            tasks_logger.addHandler(handler)
+            
+            test = tests[name]
+            try:
+                scenario = SCENARIOS[test["scenario"]]()
+                description = scenario.execute(test, experiment_id)
+                results["test_runs"][test["name"]][experiment_id] = description["result"]
+            except:
+                chaos_logger.exception("error on running an experiment")
+                results["test_runs"][test["name"]][experiment_id] = Result.UNKNOWN
+            finally:
+                handler.flush()
+                handler.close()
+                chaos_logger.removeHandler(handler)
+                tasks_logger.removeHandler(handler)
+            
+            if results["test_runs"][test["name"]][experiment_id] == Result.FAILED:
+                passed = False
+                break
 
-        with open(f"/mnt/vectorized/experiments/{args.run_id}.json", "w") as info:
-            info.write(json.dumps(results, indent=2))
-        with open(f"/mnt/vectorized/experiments/latest.json", "w") as info:
-            info.write(json.dumps(results, indent=2))
-    
-    if most_severe_result == Result.FAILED:
-        least_severe_result = Result.FAILED
-    
-    if ignore_transient_errors:
-        if at_least_one_passes:
-            results["result"] = Result.more_severe(results["result"], least_severe_result)
-        elif least_severe_result == Result.FAILED:
+            with open(f"/mnt/vectorized/experiments/{args.run_id}.json", "w") as info:
+                info.write(json.dumps(results, indent=2))
+            with open(f"/mnt/vectorized/experiments/latest.json", "w") as info:
+                info.write(json.dumps(results, indent=2))
+            
+            if should_retry_on_transient_errors:
+                passed = results["test_runs"][test["name"]][experiment_id] == Result.PASSED
+            else:
+                most_severe_result = Result.more_severe(most_severe_result, results["test_runs"][test["name"]][experiment_id])
+                least_severe_result = Result.least_severe(least_severe_result, results["test_runs"][test["name"]][experiment_id])
+            
+            if passed:
+                break
+        
+        if not passed:
             results["result"] = Result.FAILED
-    else:
-        results["result"] = Result.more_severe(results["result"], most_severe_result)
+            with open(f"/mnt/vectorized/experiments/{args.run_id}.json", "w") as info:
+                info.write(json.dumps(results, indent=2))
+            with open(f"/mnt/vectorized/experiments/latest.json", "w") as info:
+                info.write(json.dumps(results, indent=2))
+
+
+    if not(should_retry_on_transient_errors):
+        if most_severe_result == Result.FAILED:
+            least_severe_result = Result.FAILED
+        if ignore_transient_errors:
+            if at_least_one_passes:
+                results["result"] = Result.more_severe(results["result"], least_severe_result)
+            elif least_severe_result == Result.FAILED:
+                results["result"] = Result.FAILED
+        else:
+            results["result"] = Result.more_severe(results["result"], most_severe_result)
     
     with open(f"/mnt/vectorized/experiments/{args.run_id}.json", "w") as info:
         info.write(json.dumps(results, indent=2))
