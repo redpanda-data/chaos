redpanda-data/chaos

commit 6f1fc83f4b2b196b8bdee021257d9663a036f00e

Wait for controller before decommissioning
"tx-single-reads-writes / java / recycle all" was failing because
it didn't handle a case in which a decommissioning request was
rejected (during controller's leadership change)

fixes https://github.com/redpanda-data/redpanda-jepsen/issues/25

harness/chaos/faults/recycle_all.py
@@ -27,6 +27,7 @@ class RecycleAllFault(OneoffFault):
             raise Exception("cant find free host")
         
         timeout_s=self.read_config(["timeout_s"], 120)
+        controller_timeout_s=self.read_config(["controller_timeout_s"], 10)
 
         nodes = list(scenario.redpanda_cluster.nodes)
         random.shuffle(nodes)
@@ -40,6 +41,8 @@ class RecycleAllFault(OneoffFault):
             node = scenario.redpanda_cluster.add_node(candidate, node_id)
             scenario.redpanda_cluster.launch(node)
 
+            controller = scenario.redpanda_cluster.wait_leader("controller", namespace="redpanda", timeout_s=controller_timeout_s)
+            logger.debug(f"controller id={controller.id} ip={controller.ip}")
             logger.debug(f"decommissioning id={target.id} ip={target.ip}")
             scenario.redpanda_cluster.admin_decommission(target, target)
             begin = time.time()
@@ -69,11 +72,14 @@ class RecycleAllFault(OneoffFault):
 
     def decommission_add(self, scenario):
         timeout_s=self.read_config(["timeout_s"], 120)
+        controller_timeout_s=self.read_config(["controller_timeout_s"], 10)
 
         nodes = list(scenario.redpanda_cluster.nodes)
         random.shuffle(nodes)
 
         for target in nodes:
+            controller = scenario.redpanda_cluster.wait_leader("controller", namespace="redpanda", timeout_s=controller_timeout_s)
+            logger.debug(f"controller id={controller.id} ip={controller.ip}")
             logger.debug(f"decommissioning id={target.id} ip={target.ip}")
             scenario.redpanda_cluster.admin_decommission(target, target)
             begin = time.time()

commit cb3e9e0550a6f41900d77e63d1f04594fb95e249

Update recycle all fault use universal timeouts
Timeout introduced in https://github.com/redpanda-data/chaos/pull/35
was not enough to let a controller become a leader after adding and
removing a node from the cluster.

Switching to the timeouts we use on the cluster start up

harness/chaos/faults/recycle_all.py
@@ -9,6 +9,9 @@ import random
 from chaos.faults.types import FaultType
 from chaos.types import TimeoutException
 from chaos.faults.fault import OneoffFault
+from chaos.scenarios.consts import NODE_ALIVE_AFTER_RESTART_S
+from chaos.scenarios.consts import STABLE_VIEW_AFTER_RESTART_S
+from chaos.scenarios.consts import CONTROLLER_AVAILABLE_AFTER_RESTART_S
 
 logger = logging.getLogger("chaos")
 
@@ -27,7 +30,7 @@ class RecycleAllFault(OneoffFault):
             raise Exception("cant find free host")
         
         timeout_s=self.read_config(["timeout_s"], 120)
-        controller_timeout_s=self.read_config(["controller_timeout_s"], 10)
+        controller_timeout_s=self.read_config(["controller_timeout_s"], CONTROLLER_AVAILABLE_AFTER_RESTART_S)
 
         nodes = list(scenario.redpanda_cluster.nodes)
         random.shuffle(nodes)
@@ -40,6 +43,8 @@ class RecycleAllFault(OneoffFault):
             logger.info(f"adding id={node_id} ip={candidate.ip}")
             node = scenario.redpanda_cluster.add_node(candidate, node_id)
             scenario.redpanda_cluster.launch(node)
+            scenario.redpanda_cluster.wait_alive(node=node, timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+            scenario.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
 
             controller = scenario.redpanda_cluster.wait_leader("controller", namespace="redpanda", timeout_s=controller_timeout_s)
             logger.debug(f"controller id={controller.id} ip={controller.ip}")
@@ -72,7 +77,7 @@ class RecycleAllFault(OneoffFault):
 
     def decommission_add(self, scenario):
         timeout_s=self.read_config(["timeout_s"], 120)
-        controller_timeout_s=self.read_config(["controller_timeout_s"], 10)
+        controller_timeout_s=self.read_config(["controller_timeout_s"], CONTROLLER_AVAILABLE_AFTER_RESTART_S)
 
         nodes = list(scenario.redpanda_cluster.nodes)
         random.shuffle(nodes)
@@ -109,6 +114,8 @@ class RecycleAllFault(OneoffFault):
             node = scenario.redpanda_cluster.add_node(target.host, node_id)
             logger.info(f"adding id={node.id} ip={node.ip}")
             scenario.redpanda_cluster.launch(node)
+            scenario.redpanda_cluster.wait_alive(node=node, timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+            scenario.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
     
     def execute(self, scenario):
         if len(scenario.redpanda_cluster.nodes) == 3:

harness/chaos/redpanda_static_cluster.py
@@ -205,9 +205,9 @@ class RedpandaCluster:
                 ssh("ubuntu@" + node.ip, "/mnt/vectorized/control/redpanda.config.sh", key, settings[key])
             self.launch(node, log_levels)
 
-    def wait_alive(self, timeout_s=10):
+    def wait_alive(self, node=None, timeout_s=10):
         begin = time.time()
-        for node in self.nodes:
+        def wait_node_alive(node):
             while True:
                 if time.time() - begin > timeout_s:
                     raise TimeoutException(f"redpanda process isn't running withing {timeout_s} sec")
@@ -215,6 +215,11 @@ class RedpandaCluster:
                 if self.is_alive(node):
                     break
                 sleep(1)
+        if node != None:
+            wait_node_alive(node)
+        else:
+            for node in self.nodes:
+                wait_node_alive(node)
     
     def brokers(self):
         return ",".join([x.ip+":9092" for x in self.hosts])

harness/chaos/scenarios/consts.py
@@ -1,0 +1,3 @@
+STABLE_VIEW_AFTER_RESTART_S = 60
+NODE_ALIVE_AFTER_RESTART_S = 10
+CONTROLLER_AVAILABLE_AFTER_RESTART_S = 30

harness/chaos/scenarios/rw_subscribe_single_fault.py
@@ -3,6 +3,9 @@ from chaos.types import TimeoutException
 from sh import mkdir
 from chaos.faults.all import FAULTS
 from chaos.workloads.all import WORKLOADS, wait_all_workloads_killed
+from chaos.scenarios.consts import NODE_ALIVE_AFTER_RESTART_S
+from chaos.scenarios.consts import STABLE_VIEW_AFTER_RESTART_S
+from chaos.scenarios.consts import CONTROLLER_AVAILABLE_AFTER_RESTART_S
 from time import sleep
 from chaos.checks.result import Result
 import copy
@@ -88,13 +91,13 @@ class RWSubscribeSingleFault(AbstractSingleFault):
                 self.redpanda_cluster.add_node(host, node_id)
 
         self.redpanda_cluster.launch_everywhere(self.read_config(["settings", "redpanda"], {}))
-        self.redpanda_cluster.wait_alive(timeout_s=10)
-        self.redpanda_cluster.get_stable_view(timeout_s=60)
+        self.redpanda_cluster.wait_alive(timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+        self.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
 
         sleep(5)
         # waiting for the controller to be up before creating a topic
         tasks_logger.info(f"waiting for the controller to be online")
-        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=30)
+        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=CONTROLLER_AVAILABLE_AFTER_RESTART_S)
 
         chaos_logger.info(f"creating \"{self.topic}\" topic with replication factor {self.replication} & {self.partitions} partitions")
         self.redpanda_cluster.create_topic(self.topic, self.replication, self.partitions)

harness/chaos/scenarios/single_topic_single_fault.py
@@ -2,13 +2,13 @@ from chaos.scenarios.abstract_single_fault import AbstractSingleFault
 from sh import mkdir
 from chaos.faults.all import FAULTS
 from chaos.workloads.all import WORKLOADS, wait_all_workloads_killed
+from chaos.scenarios.consts import NODE_ALIVE_AFTER_RESTART_S
+from chaos.scenarios.consts import STABLE_VIEW_AFTER_RESTART_S
+from chaos.scenarios.consts import CONTROLLER_AVAILABLE_AFTER_RESTART_S
 from time import sleep
 from chaos.checks.result import Result
 import copy
-from chaos.types import TimeoutException
-import sys
-import traceback
-import time
+
 
 import logging
 
@@ -89,12 +89,12 @@ class SingleTopicSingleFault(AbstractSingleFault):
                 self.redpanda_cluster.add_node(host, node_id)
 
         self.redpanda_cluster.launch_everywhere(self.read_config(["settings", "redpanda"], {}))
-        self.redpanda_cluster.wait_alive(timeout_s=10)
-        self.redpanda_cluster.get_stable_view(timeout_s=60)
+        self.redpanda_cluster.wait_alive(timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+        self.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
 
         sleep(5)
         # waiting for the controller to be up before creating a topic
-        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=30)
+        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=CONTROLLER_AVAILABLE_AFTER_RESTART_S)
 
         logger.info(f"creating \"{self.topic}\" topic with replication factor {self.replication}")
         self.redpanda_cluster.create_topic(self.topic, self.replication, 1, cleanup=self.cleanup)

harness/chaos/scenarios/tx_money_single_fault.py
@@ -2,6 +2,9 @@ from chaos.scenarios.abstract_single_fault import AbstractSingleFault
 from sh import mkdir
 from chaos.faults.all import FAULTS
 from chaos.workloads.all import WORKLOADS, wait_all_workloads_killed
+from chaos.scenarios.consts import NODE_ALIVE_AFTER_RESTART_S
+from chaos.scenarios.consts import STABLE_VIEW_AFTER_RESTART_S
+from chaos.scenarios.consts import CONTROLLER_AVAILABLE_AFTER_RESTART_S
 from time import sleep
 from chaos.checks.result import Result
 import copy
@@ -86,8 +89,8 @@ class TxMoneySingleFault(AbstractSingleFault):
 
         log_levels = self.read_config(["settings", "log-level"], { "default": "info" })
         self.redpanda_cluster.launch_everywhere(self.read_config(["settings", "redpanda"], {}), log_levels)
-        self.redpanda_cluster.wait_alive(timeout_s=10)
-        self.redpanda_cluster.get_stable_view(timeout_s=60)
+        self.redpanda_cluster.wait_alive(timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+        self.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
 
         sleep(5)
         # waiting for the controller to be up before creating a topic
@@ -95,7 +98,7 @@ class TxMoneySingleFault(AbstractSingleFault):
             "controller",
             namespace="redpanda",
             replication=len(self.redpanda_cluster.nodes),
-            timeout_s=self.read_config(["settings", "setup", "wait_leader_timeout_s", "controller"], 30))
+            timeout_s=self.read_config(["settings", "setup", "wait_leader_timeout_s", "controller"], CONTROLLER_AVAILABLE_AFTER_RESTART_S))
 
         for i in range(0, self.accounts):
             logger.info(f"creating \"acc{i}\" topic with replication factor {self.replication}")

harness/chaos/scenarios/tx_single_topic_single_fault.py
@@ -3,6 +3,9 @@ from chaos.types import TimeoutException
 from sh import mkdir
 from chaos.faults.all import FAULTS
 from chaos.workloads.all import WORKLOADS, wait_all_workloads_killed
+from chaos.scenarios.consts import NODE_ALIVE_AFTER_RESTART_S
+from chaos.scenarios.consts import STABLE_VIEW_AFTER_RESTART_S
+from chaos.scenarios.consts import CONTROLLER_AVAILABLE_AFTER_RESTART_S
 from time import sleep
 from chaos.checks.result import Result
 import copy
@@ -88,12 +91,12 @@ class TxSingleTopicSingleFault(AbstractSingleFault):
 
         log_levels = self.read_config(["settings", "log-level"], { "default": "info" })
         self.redpanda_cluster.launch_everywhere(self.read_config(["settings", "redpanda"], {}), log_levels)
-        self.redpanda_cluster.wait_alive(timeout_s=10)
-        self.redpanda_cluster.get_stable_view(timeout_s=60)
+        self.redpanda_cluster.wait_alive(timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+        self.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
 
         sleep(5)
         # waiting for the controller to be up before creating a topic
-        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=30)
+        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=CONTROLLER_AVAILABLE_AFTER_RESTART_S)
 
         cleanup = self.read_config(["settings", "cleanup"], "delete")
 

harness/chaos/scenarios/tx_subscribe_single_fault.py
@@ -4,6 +4,9 @@ from chaos.types import TimeoutException
 from sh import mkdir
 from chaos.faults.all import FAULTS
 from chaos.workloads.all import WORKLOADS, wait_all_workloads_killed
+from chaos.scenarios.consts import NODE_ALIVE_AFTER_RESTART_S
+from chaos.scenarios.consts import STABLE_VIEW_AFTER_RESTART_S
+from chaos.scenarios.consts import CONTROLLER_AVAILABLE_AFTER_RESTART_S
 from time import sleep
 from chaos.checks.result import Result
 import copy
@@ -93,13 +96,13 @@ class TxSubscribeSingleFault(AbstractSingleFault):
         log_levels = self.read_config(["settings", "log-level"], { "default": "info" })
         tasks_logger.info(f"starting redpanda cluster with {json.dumps(log_levels)}")
         self.redpanda_cluster.launch_everywhere(self.read_config(["settings", "redpanda"], {}), log_levels)
-        self.redpanda_cluster.wait_alive(timeout_s=10)
-        self.redpanda_cluster.get_stable_view(timeout_s=60)
+        self.redpanda_cluster.wait_alive(timeout_s=NODE_ALIVE_AFTER_RESTART_S)
+        self.redpanda_cluster.get_stable_view(timeout_s=STABLE_VIEW_AFTER_RESTART_S)
 
         sleep(5)
         # waiting for the controller to be up before creating a topic
         tasks_logger.info(f"waiting for the controller to be online")
-        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=30)
+        self.redpanda_cluster.wait_leader("controller", namespace="redpanda", replication=len(self.redpanda_cluster.nodes), timeout_s=CONTROLLER_AVAILABLE_AFTER_RESTART_S)
 
         chaos_logger.info(f"creating \"{self.source}\" topic with replication factor {self.replication} & {self.partitions} partitions")
         self.redpanda_cluster.create_topic(self.source, self.replication, self.partitions)