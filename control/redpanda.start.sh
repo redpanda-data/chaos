fd9067475671ae83e9afd7221004098ae403d645
Michał Maślanka <michal@redpanda.com>
2024-07-12 11:49:43 +0000

    control: silent exception and io loggers
    Signed-off-by: Michał Maślanka <michal@redpanda.com>

--- control/redpanda.start.sh
+++ control/redpanda.start.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-nohup /bin/redpanda --default-log-level $1 --logger-log-level=$2 --redpanda-cfg /etc/redpanda/redpanda.yaml --smp 1 >/mnt/vectorized/redpanda/log.$(date +%s) 2>&1 &
+nohup /bin/redpanda --default-log-level $1 --logger-log-level=$2 --logger-log-level=exception=warn --logger-log-level=io=info --redpanda-cfg /etc/redpanda/redpanda.yaml --smp 1 >/mnt/vectorized/redpanda/log.$(date +%s) 2>&1 &
 echo $! >/mnt/vectorized/redpanda/pid
